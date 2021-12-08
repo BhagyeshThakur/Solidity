369105aee27f4c922525776e6597617b9c52236e
BhagyeshThakur <68985921+BhagyeshThakur@users.noreply.github.com>
2021-12-08 16:52:57 +0530

    Create variable_types.sol

---
+++ variable_types.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 favnumber = 5;
+    bool favbool = true;
+    string favstring = "String";
+    int256 favint = -5;
+    address favaddress = 0x868fF2b29FCA964fBBdC9F173c359F45aF99b13F;
+    bytes32 favbytes = "cat";
+}
